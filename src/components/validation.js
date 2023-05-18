fb44fb4af10d4732f6f301ba00314b350dafcd0b
DmitryGolotin <102403588+GolotinaNina@users.noreply.github.com>
2023-05-18 18:23:40 +0300

    CustomValid

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,104 +1,74 @@
-// Валидация
-
 let params = {};
 
 const showInputError = (formElement, inputElement, errorMessage) => {
-  // Находим элемент ошибки внутри самой функции
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.add(params.inputErrorClass);
   errorElement.textContent = errorMessage;
   errorElement.classList.add(params.errorClass);
 };
 
 const hideInputError = (formElement, inputElement) => {
-  // Находим элемент ошибки
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.remove(params.inputErrorClass);
   errorElement.classList.remove(params.errorClass);
   errorElement.textContent = "";
 };
 
-export const isValid = (formElement, inputElement) => {
+export const isValid = (formElement, inputElement, el) => {
   if (!inputElement.validity.valid) {
-    // showInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
-    showInputError(
-      formElement,
-      inputElement,
-      inputElement.validationMessage,
-      params
-    );
+    showInputError(formElement,inputElement,inputElement.validationMessage);
+  } else if(typeof el !== "undefined" && customValid(el.data)){
+    showInputError(formElement,inputElement, 'Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы');
   } else {
-    // hideInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
     hideInputError(formElement, inputElement, params);
   }
 };
 
+const customValid = (elChar) =>{
+  return elChar != null && !(/^([a-zA-ZА-Яа-я0-9]){1,1}$/.test(elChar) || [45,32].includes(elChar.charCodeAt(0)));
+}
+
 const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
     buttonElement.disabled = true;
     buttonElement.classList.add(params.inactiveButtonClass);
   } else {
-    // иначе сделай кнопку активной
     buttonElement.disabled = false;
     buttonElement.classList.remove(params.inactiveButtonClass);
   }
 };
 
 const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
   return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся функция
-    // hasInvalidInput вернёт true
-
     return !inputElement.validity.valid;
   });
 };
 
 const setEventListeners = (formElement) => {
-  // Найдём все поля формы и сделаем из них массив
   const inputList = Array.from(
     formElement.querySelectorAll(params.inputSelector)
   );
-  // Найдём в текущей форме кнопку отправки
   const buttonElement = formElement.querySelector(params.submitButtonSelector);
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", () => {
-      isValid(formElement, inputElement, params);
-
-      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
-      toggleButtonState(inputList, buttonElement, params);
+    inputElement.addEventListener("input", (el) => {
+      isValid(formElement, inputElement, el);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
 
 export const checkButtonIsValid = (formElement) => {
-  // Найдём все поля формы и сделаем из них массив
   const inputList = Array.from(formElement.querySelectorAll(`input`));
-  // Найдём в текущей форме кнопку отправки
   const buttonElement = formElement.querySelector('button[type="submit"]');
 
   toggleButtonState(inputList, buttonElement);
 };
 
 export const enableValidation = (p) => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
   params = p;
-
   const formList = Array.from(document.querySelectorAll(params.formSelector));
-
-  // Переберём полученную коллекцию
   formList.forEach((formElement) => {
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
     setEventListeners(formElement);
   });
 };
